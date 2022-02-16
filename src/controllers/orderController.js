74110aba188093be2035a4c0c40ff085d72bf71a
Eric <menaericdaniel@gmail.com>
2022-02-16 13:25:00 -0300

    fix email admin

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -1,16 +1,15 @@
 const xl = require("excel4node");
 const moment = require("moment");
-const {SMTPClient, Message} = require('emailjs');
+const { SMTPClient, Message } = require("emailjs");
 const { Op } = require("sequelize");
 
 const db = require("../database/models");
 
-
 const client = new SMTPClient({
-	user: 'cotizadorblancomad@gmail.com',
-	password: 'cotizador2022',
-	host: 'smtp.gmail.com',
-	ssl: true,
+  user: "cotizadorblancomad@gmail.com",
+  password: "cotizador2022",
+  host: "smtp.gmail.com",
+  ssl: true,
 });
 
 module.exports = {
@@ -158,6 +157,7 @@ module.exports = {
 
         let wb = new xl.Workbook();
         let ws = wb.addWorksheet("Orden: " + new Date().getTime());
+
         let style = wb.createStyle({
           font: {
             color: "#666666",
@@ -175,7 +175,7 @@ module.exports = {
             },
           });
         ws.cell(2, 1, 2, 5, true)
-          .string(`PEDIDO PARA : ${references.join(', ')}`)
+          .string(`PEDIDO PARA : ${references.join(", ")}`)
           .style(style)
           .style({
             alignment: {
@@ -185,24 +185,24 @@ module.exports = {
           });
 
         ws.cell(1, 6, 2, 10, true)
-        .string(`OBSERVACIONES PEDIDO : ${order.observations}`)
-        .style(style)
-        .style({
-        alignment: {
-            vertical: "center",
-            horizontal: "left",
-        },
-        });
+          .string(`OBSERVACIONES PEDIDO : ${order.observations}`)
+          .style(style)
+          .style({
+            alignment: {
+              vertical: "center",
+              horizontal: "left",
+            },
+          });
 
         ws.cell(1, 11, 2, 15, true)
-        .string(`FECHA PEDIDO : ${moment().format('DD/MM/YY')}`)
-        .style(style)
-        .style({
-        alignment: {
-            vertical: "center",
-            horizontal: "left",
-        },
-        });
+          .string(`FECHA PEDIDO : ${moment().format("DD/MM/YY")}`)
+          .style(style)
+          .style({
+            alignment: {
+              vertical: "center",
+              horizontal: "left",
+            },
+          });
 
         ws.row(1).setHeight(30);
         ws.row(2).setHeight(30);
@@ -270,235 +270,260 @@ module.exports = {
           ws.cell(index, 15)
             .number(quotation.amount * quotation.quantity)
             .style(style);
-        lastRow = index + 1;
+          lastRow = index + 1;
         });
 
         ws.cell(lastRow, 14, lastRow, 14, true)
-        .string(`EMBALAJE: `)
-        .style(style)
-        .style({
-        alignment: {
-            vertical: "center",
-            horizontal: "right",
-        },
-        });
+          .string(`EMBALAJE: `)
+          .style(style)
+          .style({
+            alignment: {
+              vertical: "center",
+              horizontal: "right",
+            },
+          });
         ws.cell(lastRow, 15, lastRow, 15, true)
-        .number(order.packaging)
-        .style(style)
-        .style({
-        alignment: {
-            vertical: "center",
-            horizontal: "right",
-        },
-        });
+          .number(order.packaging)
+          .style(style)
+          .style({
+            alignment: {
+              vertical: "center",
+              horizontal: "right",
+            },
+          });
 
         const amounts = order.quotations.map(
-            (quotation) => quotation.amount * quotation.quantity
-          );
-  
+          (quotation) => quotation.amount * quotation.quantity
+        );
+
         const total = amounts.reduce((acum, num) => acum + num);
 
         ws.cell(lastRow + 1, 14, lastRow + 1, 14, true)
-        .string(`TOTAL:`)
-        .style(style)
-        .style({
-        alignment: {
-            vertical: "center",
-            horizontal: "right",
-        },
-        });
-        ws.cell(lastRow + 1 , 15, lastRow +1, 15, true)
-        .number(total + order.packaging)
-        .style(style)
-        .style({
-        alignment: {
-            vertical: "center",
-            horizontal: "right",
-        },
-        });
+          .string(`TOTAL:`)
+          .style(style)
+          .style({
+            alignment: {
+              vertical: "center",
+              horizontal: "right",
+            },
+          });
+        ws.cell(lastRow + 1, 15, lastRow + 1, 15, true)
+          .number(total + order.packaging)
+          .style(style)
+          .style({
+            alignment: {
+              vertical: "center",
+              horizontal: "right",
+            },
+          });
 
         var fileClient = `${new Date().getTime()}.xlsx`;
 
         wb.write(`src/downloads/${fileClient}`);
 
         //planilla admin
-        let ws2 = wb.addWorksheet("Orden: " + new Date().getTime() + " - admin")
-      
-          ws2.cell(1, 1, 1, 5, true)
-            .string(`VENDEDOR: ${order.user.name} ${order.user.surname}`)
-            .style(style)
-            .style({
-              alignment: {
-                vertical: "center",
-                horizontal: "left",
-              },
-            });
-          ws2.cell(2, 1, 2, 5, true)
-            .string(`PEDIDO PARA : ${references.join(', ')}`)
-            .style(style)
-            .style({
-              alignment: {
-                vertical: "center",
-                horizontal: "left",
-              },
-            });
-  
-          ws2.cell(1, 6, 2, 10, true)
+        let wbAdmin = new xl.Workbook();
+
+        let ws2 = wbAdmin.addWorksheet(
+          "Orden: " + new Date().getTime() + " - admin"
+        );
+
+        ws2
+          .cell(1, 1, 1, 5, true)
+          .string(`VENDEDOR: ${order.user.name} ${order.user.surname}`)
+          .style(style)
+          .style({
+            alignment: {
+              vertical: "center",
+              horizontal: "left",
+            },
+          });
+        ws2
+          .cell(2, 1, 2, 5, true)
+          .string(`PEDIDO PARA : ${references.join(", ")}`)
+          .style(style)
+          .style({
+            alignment: {
+              vertical: "center",
+              horizontal: "left",
+            },
+          });
+
+        ws2
+          .cell(1, 6, 2, 10, true)
           .string(`OBSERVACIONES PEDIDO : ${order.observations}`)
           .style(style)
           .style({
-          alignment: {
+            alignment: {
               vertical: "center",
               horizontal: "left",
-          },
+            },
           });
-  
-          ws2.cell(1, 11, 2, 15, true)
-          .string(`FECHA PEDIDO : ${moment().format('DD/MM/YY')}`)
+
+        ws2
+          .cell(1, 11, 2, 15, true)
+          .string(`FECHA PEDIDO : ${moment().format("DD/MM/YY")}`)
           .style(style)
           .style({
-          alignment: {
+            alignment: {
               vertical: "center",
               horizontal: "left",
-          },
+            },
           });
-  
-          ws2.row(1).setHeight(30);
-          ws2.row(2).setHeight(30);
-          ws2.column(1).setWidth(5); //cantidad
-          ws2.column(2).setWidth(15); //sistema
-          ws2.column(3).setWidth(15); //tela
-          ws2.column(4).setWidth(15); //color
-          ws2.column(5).setWidth(15); //ancho
-          ws2.column(6).setWidth(15); //alto
-          ws2.column(7).setWidth(15); //modelo
-          ws2.column(8).setWidth(15); //cadena
-          ws2.column(9).setWidth(15); //soporte
-          ws2.column(10).setWidth(15); //comando
-          ws2.column(11).setWidth(15); //o. soporte
-          ws2.column(12).setWidth(15); //o. tela
-          ws2.column(13).setWidth(15); //observaciones
-          ws2.column(14).setWidth(15); //p. unitario
-          ws2.column(15).setWidth(15); //total
-  
-          titles.forEach((title, index) => {
-            ws2.cell(3, index + 1)
-              .string(title)
-              .style(style)
-              .style({
-                font: {
-                  bold: true,
-                },
-              });
-          });
-         
-          order.quotations.forEach((quotation, index) => {
-            index = index + 4;
-            ws2.cell(index, 1).number(quotation.quantity).style(style);
-            ws2.cell(index, 2).number(quotation.systemId).style(style);
-            ws2.cell(index, 3).number(quotation.clothId).style(style);
-            ws2.cell(index, 4).number(quotation.colorId).style(style);
-            ws2.cell(index, 5).number(quotation.clothWidth).style(style);
-            ws2.cell(index, 6).number(quotation.heigth).style(style);
-            ws2.cell(index, 7).number(quotation.patternId).style(style);
-            ws2.cell(index, 8).number(quotation.chainId).style(style);
-            ws2.cell(index, 9).number(quotation.supportId).style(style);
-            ws2.cell(index, 10).string(quotation.command).style(style);
-            ws2.cell(index, 11).string(quotation.supportOrientation).style(style);
-            ws2.cell(index, 12).string(quotation.clothOrientation).style(style);
-            ws2.cell(index, 13).string(quotation.observations).style(style);
-            ws2.cell(index, 14).number(quotation.amount).style(style);
-            ws2.cell(index, 15)
-              .number(quotation.amount * quotation.quantity)
-              .style(style);
+
+        ws2.row(1).setHeight(30);
+        ws2.row(2).setHeight(30);
+        ws2.column(1).setWidth(5); //cantidad
+        ws2.column(2).setWidth(15); //sistema
+        ws2.column(3).setWidth(15); //tela
+        ws2.column(4).setWidth(15); //color
+        ws2.column(5).setWidth(15); //ancho
+        ws2.column(6).setWidth(15); //alto
+        ws2.column(7).setWidth(15); //modelo
+        ws2.column(8).setWidth(15); //cadena
+        ws2.column(9).setWidth(15); //soporte
+        ws2.column(10).setWidth(15); //comando
+        ws2.column(11).setWidth(15); //o. soporte
+        ws2.column(12).setWidth(15); //o. tela
+        ws2.column(13).setWidth(15); //observaciones
+        ws2.column(14).setWidth(15); //p. unitario
+        ws2.column(15).setWidth(15); //total
+
+        titles.forEach((title, index) => {
+          ws2
+            .cell(3, index + 1)
+            .string(title)
+            .style(style)
+            .style({
+              font: {
+                bold: true,
+              },
+            });
+        });
+
+        order.quotations.forEach((quotation, index) => {
+          index = index + 4;
+          ws2.cell(index, 1).number(quotation.quantity).style(style);
+          ws2.cell(index, 2).number(quotation.systemId).style(style);
+          ws2.cell(index, 3).number(quotation.clothId).style(style);
+          ws2.cell(index, 4).number(quotation.colorId).style(style);
+          ws2.cell(index, 5).number(quotation.clothWidth).style(style);
+          ws2.cell(index, 6).number(quotation.heigth).style(style);
+          ws2.cell(index, 7).number(quotation.patternId).style(style);
+          ws2.cell(index, 8).number(quotation.chainId).style(style);
+          ws2.cell(index, 9).number(quotation.supportId).style(style);
+          ws2.cell(index, 10).string(quotation.command).style(style);
+          ws2.cell(index, 11).string(quotation.supportOrientation).style(style);
+          ws2.cell(index, 12).string(quotation.clothOrientation).style(style);
+          ws2.cell(index, 13).string(quotation.observations).style(style);
+          ws2.cell(index, 14).number(quotation.amount).style(style);
+          ws2
+            .cell(index, 15)
+            .number(quotation.amount * quotation.quantity)
+            .style(style);
           lastRow = index + 1;
-          });
-  
-          ws2.cell(lastRow, 14, lastRow, 14, true)
+        });
+
+        ws2
+          .cell(lastRow, 14, lastRow, 14, true)
           .string(`EMBALAJE: `)
           .style(style)
           .style({
-          alignment: {
+            alignment: {
               vertical: "center",
               horizontal: "right",
-          },
+            },
           });
-          ws2.cell(lastRow, 15, lastRow, 15, true)
+        ws2
+          .cell(lastRow, 15, lastRow, 15, true)
           .number(order.packaging)
           .style(style)
           .style({
-          alignment: {
+            alignment: {
               vertical: "center",
               horizontal: "right",
-          },
+            },
           });
-  
-          ws2.cell(lastRow + 1, 14, lastRow + 1, 14, true)
+
+        ws2
+          .cell(lastRow + 1, 14, lastRow + 1, 14, true)
           .string(`TOTAL:`)
           .style(style)
           .style({
-          alignment: {
+            alignment: {
               vertical: "center",
               horizontal: "right",
-          },
+            },
           });
-          ws2.cell(lastRow + 1 , 15, lastRow +1, 15, true)
+        ws2
+          .cell(lastRow + 1, 15, lastRow + 1, 15, true)
           .number(total + order.packaging)
           .style(style)
           .style({
-          alignment: {
+            alignment: {
               vertical: "center",
               horizontal: "right",
-          },
+            },
           });
-          
-          var fileAdmin = `${new Date().getTime()}.xlsx`;
-  
-          await db.Order.update(
-              {
-                fileClient,
-                fileAdmin
-              },
-              {
-                where: {
-                  id: +req.query.order,
-                },
-              }
-            );
-  
-          wb.write(`src/downloads/${fileAdmin}`);
+
+        var fileAdmin = `${new Date().getTime()}.xlsx`;
+
+        await db.Order.update(
+          {
+            fileClient,
+            fileAdmin,
+          },
+          {
+            where: {
+              id: +req.query.order,
+            },
+          }
+        );
+
+        wbAdmin.write(`src/downloads/${fileAdmin}`);
 
         setTimeout(() => {
+          let message = new Message({
+            text: `Hola, ${req.session.userLogin.name}.\nSe adjunta planilla con los datos de la orden generada. Gracias por usar nuestra aplicación.`,
+            from: "cotizadorblancomad@gmail.com",
+            to: req.session.userLogin.email,
+            cc: " ",
+            subject: "Orden | Presupuesto",
+            attachment: [
+              {
+                path: `src/downloads/${fileClient}`,
+                type: "application/octet-stream",
+                name: fileClient,
+              },
+            ],
+          });
+          let message2 = new Message({
+            text: `Se adjunta planilla de la orden con ID ${order.id}.\nVendedor/a: ${req.session.userLogin.name}.`,
+            from: "cotizadorblancomad@gmail.com",
+            to: "cotizadorblancomad@gmail.com",
+            cc: " ",
+            subject: "Orden | Presupuesto",
+            attachment: [
+              {
+                path: `src/downloads/${fileClient}`,
+                type: "application/octet-stream",
+                name: fileClient,
+              },
+              {
+                path: `src/downloads/${fileAdmin}`,
+                type: "application/octet-stream",
+                name: fileAdmin,
+              },
+            ],
+          });
 
-            let message = new Message({
-                text: `Hola, ${req.session.userLogin.name}.\nSe adjunta planilla con los datos de la orden generada. Gracias por usar nuestra aplicación.`,
-                from: 'cotizadorblancomad@gmail.com',
-                to: req.session.userLogin.email,
-                cc: ' ',
-                subject: 'Orden | Presupuesto',
-                attachment: [
-                    { path: `src/downloads/${fileClient}`, type: 'application/octet-stream', name: fileClient },
-                ],
-            });
-            let message2 = new Message({
-                text: `Se adjunta planilla de la orden con ID ${order.id}.\nVendedor/a: ${req.session.userLogin.name}.`,
-                from: 'cotizadorblancomad@gmail.com',
-                to: 'cotizadorblancomad@gmail.com',
-                cc: ' ',
-                subject: 'Orden | Presupuesto',
-                attachment: [
-                    { path: `src/downloads/${fileAdmin}`, type: 'application/octet-stream', name: fileAdmin },
-                ],
-            });
+          client.send(message, (err, message) => {
+            console.log(err || message);
+          });
 
-            client.send(message, (err, message) => {
-                console.log(err || message);
-            });
-
-            client.send(message2, (err, message) => {
-                console.log(err || message);
-            });
-
+          client.send(message2, (err, message) => {
+            console.log(err || message);
+          });
 
           return res.redirect("/");
         }, 2000);
@@ -507,9 +532,7 @@ module.exports = {
       console.log(error);
     }
   },
-  createExcel: (req, res) => {
-
-  },
+  createExcel: (req, res) => {},
   detail: (req, res) => {
     res.render("orderDetail");
   },
