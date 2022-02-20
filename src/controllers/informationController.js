44cb504147158e1fd2a5ac8e0672b4a66cdb1681
Emanuel Arroyo <ema201217@gmail.com>
2022-02-20 19:58:26 -0300

    --

--- src/controllers/informationController.js
+++ src/controllers/informationController.js
@@ -1,7 +1,3 @@
-const db = require('../database/models');
-const {Op} = require('sequelize');
-const { render } = require('../app');
-
 module.exports = {
     backout: (req, res) => {
         res.render('messagePremiumStandard')
