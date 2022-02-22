be220ec37499d456546b71e898f76462faa688d7
Emanuel Arroyo <emanuelarroyodev@gmail.com>
2022-02-22 07:12:26 -0300

    Completed view and functioning of width of cloth

--- src/controllers/informationController.js
+++ src/controllers/informationController.js
@@ -1,5 +1,30 @@
+const db = require("../database/models");
+const { Op } = require("sequelize");
 module.exports = {
-    backout: (req, res) => {
-        res.render('messagePremiumStandard')
-    },
-}
+  backout: (req, res) => {
+    res.render("messagePremiumStandard");
+  },
+  width: async (req, res) => {
+    const items = await db.Cloth.findAll({ where: { visible: true } });
+    res.render("clothWidth", { items, total: items.length });
+  },
+  search: async (req, res) => {
+    const items = await db.Cloth.findAll({
+      where: {
+        [Op.and]: [
+          { visible: true },
+          {
+            name: {
+              [Op.like]: `%${req.query.search}%`,
+            },
+          },
+        ],
+      },
+    });
+    res.render("clothWidth", {
+      items,
+      keywords: req.query.search,
+      total: items.length,
+    });
+  },
+};
--- src/routes/information.js
+++ src/routes/information.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const {backout} = require('../controllers/informationController');
+const {backout,width,search} = require('../controllers/informationController');
 
 router
     .get('/backout', backout)
-   
+    .get('/cloth-width', width)
+    .get('/cloth-width/search',search)
 
 module.exports = router;
