884304f28c7ebf4e32dca6c0bccf36ef1107890d
Emanuel Arroyo <emanuelarroyodev@gmail.com>
2022-02-26 09:09:04 -0300

    I add wiew width cloth

--- src/controllers/informationController.js
+++ src/controllers/informationController.js
@@ -2,4 +2,15 @@ module.exports = {
     backout: (req, res) => {
         res.render('messagePremiumStandard')
     },
+    clothWidth: (req, res) => {
+      const items = [
+        {name:"Blackout Premium / Screen 5% / Venecia Blackout / Monaco / Marsella / Paris",width:2.50},
+        {name:"Kenia / Noruega / Belgica natural y marron / Ibiza / Congo / Marsella",width:2.00},
+        {name:"Eclipse lino",width:2.00},
+        {name:"Eclipse blanco",width:1.60},
+        {name:"Blackout Standard",width:1.60},
+        {name:"Blackout Blanco",width:1.30}
+    ]
+      res.render("clothWidth", { items });
+    },
 }
--- src/routes/information.js
+++ src/routes/information.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const {backout} = require('../controllers/informationController');
+const {backout,clothWidth} = require('../controllers/informationController');
 
 router
     .get('/backout', backout)
+    .get('/cloth-width', clothWidth)
    
 
 module.exports = router;
