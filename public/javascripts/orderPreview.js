c9384842a21272f83dfc859298b05d8093ea4ef7
Eric <menaericdaniel@gmail.com>
2022-02-26 12:04:45 -0300

    add alert send order

--- public/javascripts/orderPreview.js
+++ public/javascripts/orderPreview.js
@@ -2,20 +2,42 @@ const observations = $('observations');
 
 window.addEventListener('load', () => {
 
-    if(sessionStorage.getItem('observations')){
+    if (sessionStorage.getItem('observations')) {
         observations.value = sessionStorage.getItem('observations')
     }
 })
 
-observations.addEventListener('keyup', ({target}) => {
+observations.addEventListener('keyup', ({ target }) => {
     console.log(target.value)
     sessionStorage.setItem('observations', target.value);
 
 })
 
-document.getElementById('form-sendOrder').addEventListener('submit', () => {
-    sessionStorage.removeItem('orderInProcess');
-    sessionStorage.removeItem('observations');
-    localStorage.removeItem('selected');
-    localStorage.removeItem('dataOrder');
+document.getElementById('form-sendOrder').addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    Swal.fire({
+        title: '¿Confirma los datos?',
+        text: "Revise la información antes de ser enviada",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#8B0000',
+        cancelButtonColor: '#C0C0C0',
+        confirmButtonText: 'Enviar pedido'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            Swal.fire(
+                '¡Pedido enviado!',
+                'Recibirá en su email la información enviada a la fábrica',
+                'success'
+            )
+            sessionStorage.removeItem('orderInProcess');
+            sessionStorage.removeItem('observations');
+            localStorage.removeItem('selected');
+            localStorage.removeItem('dataOrder');
+            e.target.submit()
+
+        }
+    })
+
 })
