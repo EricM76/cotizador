b266aa3d09ff9b52c1455a4906aa97b51559b5cb
Eric <menaericdaniel@gmail.com>
2022-07-29 11:29:33 -0300

    change response order send

--- src/controllers/responseController.js
+++ src/controllers/responseController.js
@@ -2,7 +2,7 @@ module.exports = {
     sendOrder : (req,res) => {
         return res.render('success', {
             title : '¡Pedido enviado con éxito!',
-            text : 'Recibirá un mail con el detalle del pedido',
+            text : 'A la brevedad recibirá un e-mail con el detalle del pedido. En caso de no recibirlo, comuníquese con la fábrica.',
             icon : 'success'
         })
     }
