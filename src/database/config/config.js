bae87cc6ef2cce2430a4e618c4b7cc9a9da2bdb6
Eric <menaericdaniel@gmail.com>
2022-11-29 12:32:15 -0300

    add enviroment in config database

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -14,6 +14,7 @@ module.exports = {
     "password": process.env.DB_TEST_PASSWORD,
     "database": process.env.DB_TEST_DATABASE,
     "host": process.env.DB_TEST_HOST,
+    "port": process.env.DB_TEST_PORT,
     "dialect": "mysql"
   },
   "production": {
@@ -21,6 +22,7 @@ module.exports = {
     "password": process.env.DB_PROD_PASSWORD,
     "database": process.env.DB_PROD_DATABASE,
     "host": process.env.DB_PROD_HOST,
+    "port": process.env.DB_PROD_PORT,
     "dialect": "mysql"
   }
 }
