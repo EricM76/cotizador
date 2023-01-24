391130c2ac7a5d0830c422988a928b96b1a665c9
Eric <menaericdaniel@gmail.com>
2023-01-24 13:06:54 -0300

    fix subjet email with accesories

--- src/controllers/accessoryController.js
+++ src/controllers/accessoryController.js
@@ -928,9 +928,10 @@ module.exports = {
       },
       data: {
         sender: { 'email': 'info@blancomad.com', 'name': 'Cotizador Blancomad' },
-        subject: 'Orden #{{params.order}}',
+        subject: '{{params.userName}} {{params.userSurname}}. Orden #{{params.order}}',
         params: {
           userName: req.session.userLogin.name,
+          userSurname : req.session.userLogin.surname,
           userEmail: req.session.userLogin.email,
           order: orderNumber
         },
